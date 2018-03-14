2261ba7bbaa661f3b7dc88a26f39baa8356223b8
Pragathi Narayanaswamy <pragathi.code@gmail.com>
2018-03-14 00:12:12 -0700

    list functions with header

--- cadet/libft/cFiles/bonus/ft_lstdel.c
+++ cadet/libft/cFiles/bonus/ft_lstdel.c
@@ -1,6 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarayan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/13 18:47:18 by pnarayan          #+#    #+#             */
+/*   Updated: 2018/03/13 19:35:46 by pnarayan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	if ((*alst)-> next)
+	if ((*alst)->next)
 		ft_lstdel(&(*alst)->next, del);
 	ft_lstdelone(&(*alst), del);
 }
