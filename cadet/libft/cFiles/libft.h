5f5c7256ea04a486a0651f19491ebef890cf879c
Pragathi Narayanaswamy <pragathi.code@gmail.com>
2018-03-14 00:41:23 -0700

    libft updated

--- cadet/libft/cFiles/libft.h
+++ cadet/libft/cFiles/libft.h
@@ -6,7 +6,7 @@
 /*   By: pnarayan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 14:06:18 by pnarayan          #+#    #+#             */
-/*   Updated: 2018/03/11 00:53:22 by pnarayan         ###   ########.fr       */
+/*   Updated: 2018/03/14 00:40:55 by pnarayan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,13 @@ size_t				ft_strlcpy(char *dst, const char *src, size_t len);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strstr(const char *haystack, const char *needle);
-char				*ft_strnstr(const char *haystack, const char *needle, size_t n);
+char				*ft_strnstr(const char *haystack, const char *needle,
+		size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_strcat(char *s1, const char *s2);
 char				*ft_strncat(char *s1, const char *s2, size_t n);
-char				*ft_strlcat(char *s1, const char *s2, size_t len);
+size_t				ft_strlcat(char *dst, const char *src, size_t len);
 int					ft_atoi(const char *str);
 int					ft_isalnum(int c);
 int					ft_isalpha(int c);
@@ -81,16 +82,13 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-
-/*
- * BONUS
- */
-
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+int					ft_no_words_c(char *str, char c)
+int					ft_wordcount(char *str)
 
 #endif
